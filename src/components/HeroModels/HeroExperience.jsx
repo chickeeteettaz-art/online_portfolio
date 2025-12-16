5926c71ae3f0397308a3ba2a6d193885a3eaff6d
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2025-12-16 11:12:47 +0200

    Hero Lights component creation

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,6 +2,8 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import { useMediaQuery } from 'react-responsive';
+import { Room } from './Optimized-room';
+import HeroLights from './HeroLights';
 
 const HeroExperiencem = () => {
   const isTablet = useMediaQuery({ query: '(max-width: 1024px)' });
@@ -9,8 +11,7 @@ const HeroExperiencem = () => {
 
   return (
     <Canvas camera={{position: [0,0,15], fov:45}}>
-      <ambientLight intensity={0.2} color={"#1a1a40"}/>
-      <directionalLight position={[5,5,5]} intensity={1}/>
+      
       <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
@@ -19,11 +20,13 @@ const HeroExperiencem = () => {
         minPolarAngle={Math.PI /5}
         maxPolarAngle={Math.PI /2}
       />
-
-      <mesh>
-        <boxGeometry args={[1,1,1]}/>
-        <meshStandardMaterial color={'teal'}/>
-      </mesh>
+      <HeroLights/>
+      <group scale={isMobile? 0.7 : 1}
+        position={[0,-3.5,0]}
+        rotation={[0,-Math.PI/4,0]}>
+        <Room/>
+      </group>
+      
     </Canvas>
   )
 }
