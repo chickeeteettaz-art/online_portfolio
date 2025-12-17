eb22fc073bf10fcd73a1d01c1450d04e6711a25a
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2025-12-17 10:42:47 +0200

    Shocasesection Animations

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ShowcaseSection = () => {
+
+    const sectionRef = useRef(null);
+    const project1Reft = useRef(null);
+    const project2Reft = useRef(null);
+    const project3Reft = useRef(null);
+
+    
+
+
+
+    useGSAP(() => {
+        const projects = [project1Reft.current, project2Reft.current, project3Reft.current];
+        
+        projects.forEach((card, index) => {
+        gsap.fromTo(
+            card,
+            {
+                y:50,
+                 opacity:0
+            },
+
+            {
+                y:0, opacity:1,
+                duration:1,
+                delay: 0.3 * (index + 1),
+                ScrollTrigger:{
+                trigger: card,
+                start: 'top bottom-=100'
+            }
+        }
+        )
+    }) 
+
+
+        gsap.fromTo(project1Reft.current,
+            {opacity:0},{opacity:1},{duration:1.5});
+    },[])
+
   return (
     <>
-        <div id='work' className='app-showcase'>
+        <section ref={sectionRef} id='work' className='app-showcase'>
             <div className='w-full'>
                 <div className='showcaselayout'>
-                    <div className='first-project-wrapper'>
+                    <div className='first-project-wrapper' ref={project1Reft}>
                         <div className='image-wrapper'>
                             <img src='/images/project1.png' alt='Ryde'/>
                         </div>
@@ -17,23 +61,25 @@ const ShowcaseSection = () => {
                     </div>
 
                     <div className='project-list-wrapper overflow-hidden'>
-                        <div className='project'>
-                            <div className='image-wrapper bg-[#ffe7eb]'>
-                                <img src='/images/project3.png' alt='Foodie'/>
-                            </div>
-                            <h2 className=''>YC Directory</h2>
-                        </div>
+                        
 
-                        <div className='project'>
+                        <div className='project' ref={project2Reft}>
                             <div className='image-wrapper bg-[#ffefdb]'>
                                 <img src='/images/project2.png' alt='Foodie'/>
                             </div>
                             <h2 className=''>Library Management</h2>
                         </div>
+
+                        <div className='project' ref={project3Reft}>
+                            <div className='image-wrapper bg-[#ffe7eb]'>
+                                <img src='/images/project3.png' alt='Foodie'/>
+                            </div>
+                            <h2 className=''>YC Directory</h2>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     </>
   )
 }
