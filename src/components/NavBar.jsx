a6d169f203904895a88435237c1e0c0b0e7c3cec
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2025-12-17 11:00:29 +0200

    NavBar implementation complete

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { navLinks } from '../constants'
 
 const NavBar = () => {
+
+    const [scrolled,setScrolled] = React.useState(false)
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 10
+            setScrolled(true)
+
+        }
+        window.addEventListener('scroll',handleScroll)
+
+        return () => {window.removeEventListener('scroll',handleScroll)}
+    },[])
   return (
-    <div>NavBar</div>
+    <header className={`navbar ${scrolled ? 'scrolled' : 'not-scrolled'}`}>
+        <div className='inner'>
+            <a className='logo' href='#hero'>Zinhle | Mahlangu</a>
+            
+            <nav className='desktop'>
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.name} className='group'>
+                            <a href={link.link}>
+                                <span>{link.name}</span>
+                                <span className='underline'/>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+            <a className='contact-btn group' href='#contact'>
+                <div className='inner'>
+                    <span>Contact Me</span>
+                </div>
+            </a>
+        </div>
+    </header>
   )
 }
 
