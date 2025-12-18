0e3bfde78279d74c4b54abc68f4cc795dfc634da
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2025-12-18 09:46:50 +0200

    Feature card implentation complete

--- src/sections/FeatureCards.jsx
+++ src/sections/FeatureCards.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { abilities } from '../constants'
 
 const FeatureCards = () => {
   return (
     <>
-        <div>
-          
+        <div className='w-full padding-x-lg'>
+          <div className = "mx-auto grid-3-cols">
+            {abilities.map((ability) => (
+              <>
+                <div key={ability.title} className='card-border rounded-xl p-8 flex flex-col gap-4'>
+                  <div className='size-14 flex items-center justify-center rounded-full'>
+                    <img src={ability.imgPath} alt={ability.title} />
+                  </div>
+                  <h3 className='text-white text-2xl font-semibold mt-2'>{ability.title}</h3>
+                  <p className='text-white-50 text-lg'>{ability.desc}</p>
+                </div>
+              </> 
+            ))}
+          </div>
         </div>
     </>
   )
