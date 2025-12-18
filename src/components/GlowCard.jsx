b20114644c454631e8ba5cbd83200f8bfd07d6f9
Chicketeetta Zinhle <chickeeteettaz@gmail.com>
2025-12-18 11:09:42 +0200

    Experience section completed

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -24,7 +24,7 @@ const GlowCard = ({card,children,index}) => {
     }
 
   return (
-    <div ref={(el) => (cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)} className='card card-border titmeline-card rounded-xl p-10'>
+    <div ref={(el) => (cardRefs.current[index] = el)} onMouseMove={handleMouseMove(index)} className='card card-border timeline-card rounded-xl p-10'>
         <div className='glow'/>
 
         <div className='flex items-center gap-1 mb-5'>
